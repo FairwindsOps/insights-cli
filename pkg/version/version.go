0dde0deb9f73d24528753aeaa2c4ebedf87b48fd
Ivan Fetch <ivan@fairwinds.com>
2022-03-16 15:00:09 -0400

    Fix linting

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,18 +1,22 @@
-// version stores the Insights CLI version, making it accessible by other
+// Package version stores the Insights CLI version, making it accessible by other
 // packages.
 package version
 
 var version string = "unknown"
 var commit string = "unknown"
 
+// String returns the version and git commit, as might be used as output for a
+// `version` command-line flag.
 func String() string {
 	return "Version: " + version + " Commit: " + commit
 }
 
+// GetVersion returns the version unexported variable.
 func GetVersion() string {
 	return version
 }
 
+// GetCommit returns the commit unexported variable.
 func GetCommit() string {
 	return commit
 }
