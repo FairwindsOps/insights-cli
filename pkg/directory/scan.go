d3fa0be69e62d701f434e130bdf7a1ff733394c9
Ivan Fetch <ivan@fairwinds.com>
2022-04-12 10:27:02 -0400

    Cleanup and comment

--- pkg/directory/scan.go
+++ pkg/directory/scan.go
@@ -15,7 +15,6 @@
 package directory
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -34,15 +33,13 @@ func ScanFolder(folder string) (map[string][]string, error) {
 		return nil, err
 	}
 	if len(regoFiles) > 0 {
-		logrus.Debugf("processing all top-level .rego files in %s as 2 Insights OPA policies", folder)
+		logrus.Debugf("processing all top-level .rego files in %s as v2 Insights OPA policies", folder)
 		for _, rf := range regoFiles {
-			rfWithoutExt := strings.TrimSuffix(rf, filepath.Ext(rf))
+			policyName := strings.TrimSuffix(rf, filepath.Ext(rf))
 			rfWithDirectory := folder + "/" + rf
-			fileMap[rfWithoutExt] = append(fileMap[rfWithoutExt], rfWithDirectory)
+			fileMap[policyName] = append(fileMap[policyName], rfWithDirectory)
 		}
-		// return fileMap, nil
 	}
-	// logrus.Debugf("treating %s as a mix of V1 and V2 Insights OPA policies as there are no top-level .rego files", folder)
 	err = filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -53,11 +50,11 @@ func ScanFolder(folder string) (map[string][]string, error) {
 		if filepath.Ext(path) != ".yaml" && !strings.HasPrefix(filepath.Base(path), "policy") {
 			return nil
 		}
-		if filepath.Dir(path) == folder { // Ignore files not in a per-policy sub-dir
-			return fmt.Errorf("File %s will not be processed because it is not in a per-policy sub-directory", path)
+		if filepath.Dir(path) == folder { // Ignore already processed top-level files
+			return nil
 		}
-		directoryName := filepath.Base(filepath.Dir(path))
-		fileMap[directoryName] = append(fileMap[directoryName], path)
+		policyName := filepath.Base(filepath.Dir(path))
+		fileMap[policyName] = append(fileMap[policyName], path)
 		return nil
 	})
 	logrus.Debugf("fileScan returning file-matp: %#v\n", fileMap)
