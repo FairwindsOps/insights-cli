d5f1e3e83ebc12652068698a3cd8b402841613d7
Ivan Fetch <ivan@fairwinds.com>
2023-03-29 09:35:27 -0600

    Fix setting E2E env vars, also set them for the second place we run tests

--- .circleci/scripts/e2e-env.sh
+++ .circleci/scripts/e2e-env.sh
@@ -1,5 +1,6 @@
 # Set environment for E2E tests.
 # See also, our Vault path repo/insights-cli/env, which will populate this environment.
 
-export CI_INSIGHTS_API_URL=https://stable-main.k8s.insights.fairwinds.com
-export CI_INSIGHTS_ORGANIZATION=cli-test
+echo "Setting environment variables required by E2E tests. . ."
+echo "export CI_INSIGHTS_API_URL='https://stable-main.k8s.insights.fairwinds.com'" >>${BASH_ENV}
+echo "export CI_INSIGHTS_ORGANIZATION='cli-test'" >>${BASH_ENV}
