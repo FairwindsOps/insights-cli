9838f09c9fde2093ae08aa518b7a51619afaa14f
Robert Brennan <contact@rbren.io>
2023-06-21 10:56:46 -0400

    add more detailed opa prompt

--- pkg/openai/openai.go
+++ pkg/openai/openai.go
@@ -11,9 +11,21 @@ type Client struct {
 	debug  bool
 }
 
-const promptBoilerplate = `
-Here is an example OPA policy. Can you write a brand new OPA policy example using the 'fairwinds' package?
 
+const promptBoilerplate = `
+Can you write a brand new OPA policy example using the 'fairwinds' package?
+
+The "category" field should be one of "Security", "Efficiency", or "Reliability".
+
+The "severity" field should be a number between 0.0 and 1.0, with 1.0 indicating that the issue is very dangerous, and 0.0 indicating the issue is not bad at all.
+
+You can use the function insightsinfo() to get more details about the context:
+* insightsinfo("cluster") will return the name of the cluster being scanned
+* insightsinfo("repository") will return the name of the infrastructure-as-code repository being scanned
+* insightsinfo("context") will return one of "CI/CD", "AdmissionController", or "Agent"
+* insightsinfo("admissionRequest") will return a Kubernetes admission object when insightsinfo("context") is AdmissionController
+
+Here is an example policy that blocks any resources with the label "foo":
 <>
 package fairwinds
 
@@ -36,6 +48,7 @@ labelblock[actionItem] {
 
 The new policy you write should: `
 
+
 func NewClient(apiKey, model string, debug bool) *Client {
 	return &Client{
 		apiKey: apiKey,
@@ -45,7 +58,7 @@ func NewClient(apiKey, model string, debug bool) *Client {
 }
 
 func (cs *Client) SendPrompt(ctx context.Context, prompt string) (string, error) {
-	message := Message{Role: "user", Content: promptBoilerplate + prompt + "'"}
+	message := Message{Role: "user", Content: promptBoilerplate + prompt}
 	request := Request{Model: cs.model, Messages: []Message{message}}
 	content, err := sendRequest(ctx, cs.apiKey, request)
 	if err != nil {
